7992a43b832fe158ef84fc305140c6181b1b6aa3
Neville Tummon <nt.devs@gmail.com>
2016-01-24 11:04:49 +1100

    fixed ordering in get_queryset

--- snippets/views.py
+++ snippets/views.py
@@ -6,19 +6,20 @@ from .models import Snippet
 # Create your views here.
 class SnippetListView(ListView):
     model = Snippet
-    ordering = ('-submitted_date')
     context_object_name = 'snippets'
     template_name = 'snippet_list.html'
     paginate_by = 5
     
     def get_queryset(self):
         if not self.request.GET.get('search'):
-            return Snippet.objects.all()
+            snippets = Snippet.objects.all()
+        else:
+            search = self.request.GET['search']
+            snippets = Snippet.objects.filter(
+                Q(body__icontains=search) | Q(tags__name__in=[search])| Q(title__icontains=search))
         
-        search = self.request.GET['search']
-        return Snippet.objects.filter(
-            Q(body__icontains=search) | Q(tags__name__in=[search])| Q(title__icontains=search))
-
+        return snippets.order_by('-submitted_date')
+        
 class SnippetDetailView(DetailView):
     model = Snippet
     context_object_name = 'snippet'

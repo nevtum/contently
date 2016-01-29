099c75c7e95e2d8881c760d3211bfbb9fff4b673
Neville Tummon <nt.devs@gmail.com>
2016-01-30 00:48:07 +1100

    merged user profile and user into one admin page

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Snippet, Vote, UserProfile
 
 # Register your models here.
@@ -9,10 +12,21 @@ class SnippetAdmin(admin.ModelAdmin):
 class VoteAdmin(admin.ModelAdmin):
     list_display = ('snippet', 'voted_by')
 
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'last_active')
-    search_fields = ['bio']
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
 
-admin.site.register(UserProfile, UserProfileAdmin)
+class UserProfileAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UserProfileAdmin)
 admin.site.register(Snippet, SnippetAdmin)
 admin.site.register(Vote, VoteAdmin)
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
+
+from django.db.models.signals import post_save
+post_save.connect(create_profile, sender=User)

278028ad3c9c0e563fe03b10b8ac58c5dce60d5a
Neville Tummon <nt.devs@gmail.com>
2016-01-29 21:09:02 +1100

    views modified for non-authenticated users

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required as auth
 from .views import (
     SnippetListView,
     SnippetCreateView,
@@ -7,7 +8,7 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', SnippetListView.as_view(), name='snippets'),
-    url(r'^submitsnippet/$', SnippetCreateView.as_view(), name='new-snippet'),
+    url(r'^submitsnippet/$', auth(SnippetCreateView.as_view()), name='new-snippet'),
     url(r'^(?P<slug>[-\w]+)/$', SnippetDetailView.as_view(), name='snippet-detail'),
-    url(r'^(?P<slug>[-\w]+)/update$', SnippetUpdateView.as_view(), name='update-snippet'),
+    url(r'^(?P<slug>[-\w]+)/update$', auth(SnippetUpdateView.as_view()), name='update-snippet'),
 ]

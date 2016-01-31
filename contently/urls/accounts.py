bb8b9d8dc7b32c8bd79a36986fbe875abfd90734
Neville Tummon <nt.devs@gmail.com>
2016-01-31 15:18:19 +1100

    added views to reset password (not tested)

--- contently/urls/accounts.py
+++ contently/urls/accounts.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout
 from django.contrib.auth.decorators import login_required as auth
+from django.contrib.auth.views import (
+    login,
+    logout,
+    password_reset,
+    password_reset_done)
 
 urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout', kwargs={ 'next_page': 'snippets' }),
+    url(r'^reset/$', password_reset, name='password_reset'),
+    url(r'^reset_done/$', password_reset_done, name='password_reset_done'),
 ]
